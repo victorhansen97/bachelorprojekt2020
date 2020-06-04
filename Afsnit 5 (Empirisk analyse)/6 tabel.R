8fa850cb2267e7985fea2e41edbf36e19079fd84
victorhansen97 <66221767+victorhansen97@users.noreply.github.com>
2020-06-04 21:00:28 +0200

    Tabel 3 (appendix)

---
+++ Afsnit 5 (Empirisk analyse)/6 tabel.R
@@ -0,0 +1,23 @@
+
+#første søjle (datoer)
+colnames(rv5_short)
+
+#første søjle med data (fed)
+rv5_short
+#næste søjle med data (normal)
+rv5_mix
+
+#søjle med data (fed)
+naive_short
+#søjle med data (normal)
+naive_mix
+
+#søjle med data (fed)
+imp_short
+#søjle med data (normal)
+imprv5_mix
+
+#søjle med data (fed)
+vix_short
+#søjle med data (normal)
+vix_mix
